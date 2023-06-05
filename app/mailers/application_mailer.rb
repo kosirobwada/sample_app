3082cd2b81c6becf87234e228f1c2840773690a9
Ubuntu <ubuntu@ip-172-31-38-186.us-east-2.compute.internal>
2023-06-05 04:11:54 +0000

    Add account activation

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "kosiro131219@gmail.com"
   layout "mailer"
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,6 +38,8 @@ Rails.application.configure do
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  host = '4626bae6fc2742fa9dae1d407df84fdd.vfs.cloud9.us-east-2.amazonaws.com/'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 
   config.action_mailer.perform_caching = false
 
